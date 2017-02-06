692669e4561b084c68d757f819adabdc5a35163c
JesseWebDotCom <jessetorres@me.com>
2017-02-05 20:02:08 -0500

    Updated PlexClients component, now with working playback controls

--- custom_components/media_player/plexclients.py
+++ custom_components/media_player/plexclients.py
@@ -5,8 +5,12 @@ Here are the advantages and disadvantages of this component over the HA
 standard media_player.plex component
 
 Advantages:
-- Works with all Plex Clients (media_player.plex adds players with incorrect
-names like 'plex web safari' and never displays them)
+- No discovery needed (queries your plex server directly for connected players)
+- Control volume (media_player.plex doesn't even show the volume control)
+- Displays proper player names (i.e. Living Room Apple TV instead of Plex Web
+Safari - This is because media_player.plex grabs the name from the session
+instead of the player object)
+- Works with all Plex Clients (media_player.plex never displays the below)
     - Mobile (iPhone, iPad, etc)
     - PlexConnect (How earlier generation Apple TV's can access Plex)
 - Displays episode specific data (when playing a season, media_player.plex will
@@ -17,13 +21,34 @@ display the first episode name and show thumbnail)
 - Shows disconnected clients as idle (when a player disconnects,
 media_player.plex keeps the player as 'playing' and adds a hidden player as
 'idle'.)
+- No configurator, plex.conf, or plex token required (this probably works for
+me because I've set Plex to NOT require authentication for my local LAN)
 
 Disadvantages:
 - Crappy, hardly tested, non-supported code
-- No media control (i.e. play, pause, volume) yet
 
-So this is my crappy way to see Plex clients connected to my Plex server.
-If you make this better, please share with me
+Here you can see a visual comparison between media_player.plex and this component:
+https://github.com/JesseWebDotCom/home-assistant-configuration/blob/master/docs/images/plex_plexclients_comparison.png
+
+To Do:
+- Clean up code
+- Resolve local names (i.e. "Jesse's Macbook" instead of "Plex Web (Safari)"")
+- Dynamically create an HA group with all players
+    - Separate the group between local and remote (using plex_client['player_location'])
+- Remove playback control (ex. SUPPORT_PLEXCLIENT = None) per player type
+
+Limitations:
+- Can't retrieve current volume (but can set)
+- No functional playback controls on PlexConnect clients
+- No functional playback controls on web clients (probably same reason above)
+- No functional playback controls on ios clients (apple thing?)
+
+
+Feel free to improve this code and even incorporate into HA (which I do not have the
+time to do) - I just ask that you let me know (so I can add your improvements or
+migrate to something better).
+
+Thanks
 
 """
 
@@ -49,10 +74,7 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'plexclients'
 
 SUPPORT_PLEXCLIENT = SUPPORT_PAUSE | SUPPORT_PREVIOUS_TRACK | SUPPORT_NEXT_TRACK | \
-    SUPPORT_STOP | SUPPORT_VOLUME_SET | SUPPORT_PLAY
-
-# Hide controls for now
-SUPPORT_PLEXCLIENT = None
+    SUPPORT_STOP | SUPPORT_PLAY | SUPPORT_VOLUME_SET
 
 DEFAULT_HOST = 'localhost'
 DEFAULT_PORT = 32400
@@ -66,18 +88,21 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
 })
 
+def get_plexresponse(url):
+    import requests
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    return requests.get(url, verify=False)
+
 def get_plexclients(plex_server_url):
     clients_url =  '{}/status/sessions'.format(plex_server_url)
 
     _LOGGER.info('Getting plex clients: ' + clients_url)
 
-    import requests
-    from requests.packages.urllib3.exceptions import InsecureRequestWarning
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    response = get_plexresponse(clients_url)
+
     import xml.etree.ElementTree as ET
-
-    response = requests.get(clients_url, verify=False)
-
     root = ET.fromstring(response.text)
 
     plex_clients = {}
@@ -108,6 +133,7 @@ def get_plexclients(plex_server_url):
             plex_client['tv_episode_number'] = session.attrib['index']
 
         plex_client['media_year'] = session.attrib['year']
+        plex_client['media_duration'] = session.attrib['duration']
         plex_client['media_name'] = session.attrib['title']
         plex_client['media_id'] = session.attrib['ratingKey']
         plex_client['media_thumbnail'] = plex_client['plex_server_url'] + session.attrib['thumb']
@@ -140,8 +166,6 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
 def setup_plexserver(host, port, hass, add_devices_callback):
 
     plex_server_url = 'http://{}:{}'.format(host, port)
-
-    _LOGGER.info('Connected to: http://%s', host)
 
     all_clients = {}
     track_utc_time_change(hass, lambda now: update_clients(), second=30)
@@ -257,6 +281,11 @@ class PlexClient(MediaPlayerDevice):
         return self._client['tv_episode_number']
 
     @property
+    def media_duration(self):
+        """Duration of current playing media in seconds."""
+        return self._client['media_duration']
+
+    @property
     def media_image_url(self):
         """Image url of current playing media."""
         return self._client['media_thumbnail']
@@ -268,28 +297,34 @@ class PlexClient(MediaPlayerDevice):
 
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
-        # self.device.setVolume(int(volume * 100),
-        #                       self._active_media_plexapi_type)
+        str_volume = str(int(volume * 100))
+        print("volume=" + str_volume)
+        _LOGGER.info("volume=" + str_volume)
+        self._playback('setParameters?volume='+str_volume)
+
+    def _playback(self, command):
+        url = '{}/system/players/{}/playback/{}'.format(self._client['plex_server_url'], self._client['player_address'], command)
+        self.get_plexresponse(url)
 
     def media_play(self):
         """Send play command."""
-        #self.device.play(self._active_media_plexapi_type)
+        self._playback('play')
 
     def media_pause(self):
         """Send pause command."""
-        #self.device.pause(self._active_media_plexapi_type)
+        self._playback('pause')
 
     def media_stop(self):
         """Send stop command."""
-        #self.device.stop(self._active_media_plexapi_type)
+        self._playback('stop')
 
     def media_next_track(self):
         """Send next track command."""
-        #self.device.skipNext(self._active_media_plexapi_type)
+        self._playback('skipNext')
 
     def media_previous_track(self):
         """Send previous track command."""
-        #self.device.skipPrevious(self._active_media_plexapi_type)
+        self._playback('skipPrevious')
 
     @property
     def volume_level(self):
@@ -306,3 +341,10 @@ class PlexClient(MediaPlayerDevice):
     def update(self):
         """Get the latest details."""
         self.update_clients(no_throttle=True)
+
+    def get_plexresponse(self, url):
+        import requests
+        from requests.packages.urllib3.exceptions import InsecureRequestWarning
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+        return requests.get(url, verify=False)
